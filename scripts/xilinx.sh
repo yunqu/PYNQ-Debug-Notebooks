db74b897db3532e1ea6040d1df608b99a93ed1bb
Xilinx User <yunq@xilinx.com>
2018-09-25 23:52:29 +0000

    retire old scripts

--- scripts/xilinx.sh
+++ scripts/xilinx.sh
@@ -7,7 +7,7 @@ fi
 
 declare -A arr
 arr+=(["http_proxy"]='"http://172.20.201.1:8080/"')
-arr+=(["https_proxy"]='"https://172.20.201.1:8080/"')
+arr+=(["https_proxy"]='"http://172.20.201.1:8080/"')
 
 # change or append
 for key in ${!arr[@]};do
